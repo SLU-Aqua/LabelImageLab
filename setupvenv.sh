9e9568523da7deee230a81622bf4198742e6e945
Erik <erik@beof.se>
2023-02-03 11:40:23 +0100

    New windos setupenv

--- setupvenv.sh
+++ setupvenv.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-rm -rf venv/
-python3 -m venv venv
-venv/bin/pip3 install --upgrade pip
-venv/bin/pip3 install -r requirements.txt 
+#!/bin/bash
+
+set -e 
+
+rm -rf venv
+python.exe -m venv venv
+./venv/Scripts/python.exe -m pip install --upgrade pip 
+./venv/Scripts/pip.exe install --no-cache-dir -r requirements.txt
+
+#case "$OSTYPE" in
+#  solaris*) echo "SOLARIS" ;;
+#  darwin*)  echo "OSX" ;; 
+#  linux*)   echo "LINUX" ;;
+#  bsd*)     echo "BSD" ;;
+#  msys*)    echo "WINDOWS" ;;
+#  cygwin*)  echo "ALSO WINDOWS" ;;
+#  *)        echo "unknown: $OSTYPE" ;;
+#esac
+
+
+#rm -rf venv/
+#python3 -m venv venv
+#venv/bin/pip3 install --upgrade pip
+#venv/bin/pip3 install -r requirements.txt 
 
 
